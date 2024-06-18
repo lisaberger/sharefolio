8ba4ae05bf21795ba6a9559863099663ff081f3c
bergerli <lisa.berger@hs-augsburg.de>
2024-06-18 19:49:08 +0200

    feat(api): refactor backend to use sequelize

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -1,5 +1,4 @@
-import client from '../db/client.js';
-import passport from 'passport';
+import Account from "../models/userModel.js";
 
 const loginUser = (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
@@ -11,20 +10,32 @@ const loginUser = (req, res, next) => {
             return res.status(400).send([user, 'Cannot log in', info]);
         }
 
-        req.login(user, (err) => {
-            client
-                .query(`SELECT * from account WHERE username = '${user}'`)
-                .then((tempres) => {
-                    res.send(tempres.rows[0]);
+        req.login(user, async (err) => {
+            if (err) {
+                return next(err);
+            }
+            
+            try {
+                const loggedInUser = await Account.findOne({
+                    where: { id: user.id } // Adjust this based on your user identifier (e.g., username)
                 });
+                
+                if (!loggedInUser) {
+                    throw new Error('User not found in database');
+                }
+                
+                res.send(loggedInUser);
+            } catch (error) {
+                next(error);
+            }
         });
     })(req, res, next);
-}
+};
 
 const logoutUser = (req, res, next) => {
     req.logout();
     console.log('logged out');
     res.send();
-}
+};
 
-export { loginUser, logoutUser }
+export { loginUser, logoutUser };
--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,73 +1,64 @@
-import client from '../db/client.js';
+import Account from '../models/userModel.js';
+import EnumCategory from '../models/categoryModel.js';
+import Project from '../models/projectModel.js';
 
-const getProjects = (req, res, next) => {
-    client.query('SELECT * FROM project')
-        .then((response) => {
-            res.status(200).json(response.rows);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        });
+const getProjects = async (req, res, next) => {
+    try {
+        const projects = await Project.findAll();
+        res.status(200).json(projects);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
 
-const getProjectByName = (req, res, next) => {
+
+const getProjectByName = async (req, res, next) => {
     const projectName = req.params.name.replace(/\s+/g, '-').toLowerCase();
-    client.query(`
-        SELECT 
-            project.*, 
-            enum_category.name AS Kategorie, 
-            account.name AS Ersteller, 
-            account.username AS Author 
-        FROM 
-            project 
-        JOIN 
-            enum_category ON project.kategorie_id = enum_category.id 
-        JOIN 
-            account ON project.ersteller_id = account.id 
-        WHERE 
-            LOWER(REPLACE(project.name, ' ', '-')) = $1`, [projectName])
-        .then((response) => {
-            if (response.rows.length === 0) {
-                res.status(404).json({ error: 'Project not found' });
-            } else {
-                res.status(200).json(response.rows);
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
+    try {
+        const project = await Project.findOne({
+            where: {
+                name: projectName
+            },
+            include: [
+                { model: EnumCategory, as: 'category', attributes: ['name'] },
+                { model: Account, as: 'creator', attributes: ['name', 'username'] }
+            ]
         });
+
+        if (!project) {
+            res.status(404).json({ error: 'Project not found' });
+        } else {
+            res.status(200).json(project);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
 
-const createProject = (req, res, next) => {
+const createProject = async (req, res, next) => {
     const projectData = req.body.projectData;
-    const query = `
-        INSERT INTO project 
-            (ersteller_id, titelbild, name, beschreibung, art, tools, kategorie_id, demolink, bild1, bild2) 
-        VALUES 
-            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
-    const values = [
-        projectData.creatorId,
-        projectData.headerPath,
-        projectData.title,
-        projectData.descr,
-        projectData.art,
-        projectData.tools,
-        projectData.category,
-        projectData.link,
-        projectData.pic1Path,
-        projectData.pic2Path
-    ];
 
-    client.query(query, values)
-        .then((response) => {
-            res.sendStatus(201); // 201 Created
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
+    try {
+        const newProject = await Project.create({
+            ersteller_id: projectData.creatorId,
+            titelbild: projectData.headerPath,
+            name: projectData.title,
+            beschreibung: projectData.descr,
+            art: projectData.art,
+            tools: projectData.tools,
+            kategorie_id: projectData.category,
+            demolink: projectData.link,
+            bild1: projectData.pic1Path,
+            bild2: projectData.pic2Path
         });
+
+        res.sendStatus(201); // 201 Created
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
 
 export { getProjects, getProjectByName, createProject };
